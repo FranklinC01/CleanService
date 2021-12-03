16fa7df3f2b7a1b95b7a7b84fb08de43f29ca0f5
FranklinC01 <fcordero@unab.edu.co>
2021-12-03 10:46:26 -0500

    initial commit

--- servidor/index.js
+++ servidor/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const conectarDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const port = process.env.PORT ||3000;
 
 //creamos servidor
 
@@ -10,8 +13,16 @@ const app = express();
 conectarDB();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+
+/*app.get('/',(req, res)=>{
+    res.send('Hola Mundo');
+})*/
+
 app.use('/api/usuarios', require('./routes/usuarios'));
 
-app.listen(4000, () => {
-    console.log("server funcionando")
+
+
+app.listen(port, () => {
+    console.log("server funcionando",port)
 })
